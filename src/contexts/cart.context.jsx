8c779aff95579aaf43be0f8eef5dbe7ad6e66010
Cândido da Silva Junior <57716228+candidosilva@users.noreply.github.com>
2022-10-27 07:36:45 -0300

    Add shop page
    Add shop page

---
+++ src/contexts/cart.context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+export const CartContext = createContext({
+  cartItems: [],
+  addItemToCart: () => {},
+});
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = {
+    cartItems,
+    addItemToCart,
+  };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
