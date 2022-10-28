9fd6f45c28233cf2909c27aa59959c29479b4183
Cândido da Silva Junior <57716228+candidosilva@users.noreply.github.com>
2022-10-28 20:06:43 -0300

    styled components
    styled components

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 
-import { CartContext } from "../../contexts/cart.context";
+import { CartContext } from '../../contexts/cart.context';
 
-import "./checkout-item.styles.scss";
+import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
@@ -15,32 +15,24 @@ const CheckoutItem = ({ cartItem }) => {
   const removeItemHandler = () => removeItemToCart(cartItem);
 
   return (
-    <div className="checkout-item-container">
-      <div className="image-container">
+    <div className='checkout-item-container'>
+      <div className='image-container'>
         <img src={imageUrl} alt={`${name}`} />
       </div>
-
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={removeItemHandler}>
+      <span className='name'> {name} </span>
+      <span className='quantity'>
+        <div className='arrow' onClick={removeItemHandler}>
           &#10094;
         </div>
-
-        <span className="value">{quantity}</span>
-
-        <span className="quantity">
-          <div className="arrow" onClick={addItemHandler}>
-            &#10095;
-          </div>
-        </span>
-      </span>
-
-      <span className="price">{price}</span>
-      <span className="remove-button">
-        <div className="arrow" onClick={clearItemHandler}>
-          &#10005;
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={addItemHandler}>
+          &#10095;
         </div>
       </span>
+      <span className='price'> {price}</span>
+      <div className='remove-button' onClick={clearItemHandler}>
+        &#10005;
+      </div>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -17,14 +17,17 @@ const addCartItem = (cartItems, productToAdd) => {
 };
 
 const removeCartItem = (cartItems, cartItemToRemove) => {
+  // find the cart item to remove
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemToRemove.id
   );
 
+  // check if quantity is equal to 1, if it is remove that item from the cart
   if (existingCartItem.quantity === 1) {
     return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
   }
 
+  // return back cartitems with matching cart item with reduced quantity
   return cartItems.map((cartItem) =>
     cartItem.id === cartItemToRemove.id
       ? { ...cartItem, quantity: cartItem.quantity - 1 }
@@ -36,19 +39,19 @@ const clearCartItem = (cartItems, cartItemToClear) =>
   cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
 export const CartContext = createContext({
-  cartItems: [],
-  addItemToCart: () => {},
   isCartOpen: false,
   setIsCartOpen: () => {},
-  removeCartItem: () => {},
-  clearCartItem: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
   cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
   const [cartTotal, setCartTotal] = useState(0);
 
@@ -72,19 +75,21 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const removeItemToCart = (cartItemToRemove) =>
+  const removeItemToCart = (cartItemToRemove) => {
     setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
 
-  const clearItemFromCart = (cartItemToClear) =>
+  const clearItemFromCart = (cartItemToClear) => {
     setCartItems(clearCartItem(cartItems, cartItemToClear));
+  };
 
   const value = {
     isCartOpen,
     setIsCartOpen,
-    cartItems,
     addItemToCart,
     removeItemToCart,
     clearItemFromCart,
+    cartItems,
     cartCount,
     cartTotal,
   };
